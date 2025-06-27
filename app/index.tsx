4f8c3b2bc84c7441c7f8dcee9b563c63ac8cf2a5
johannes <johannes.muljana@ait.nsw.edu.au>
2025-06-27 10:04:14 +1000

    add validation indicator

--- app/index.tsx
+++ app/index.tsx
@@ -2,61 +2,75 @@ import { View, Text, StyleSheet, TextInput, Pressable } from 'react-native'
 import { ThemedView } from '@/components/ThemedView'
 import { ThemedText } from '@/components/ThemedText'
 import { useState, useEffect } from 'react'
+import { ValidIndicator } from '@/components/ui/ValidIndicator'
 import { Link } from 'expo-router'
 
 export default function SignUp(props: any) {
-    const [ email, setEmail ] = useState<string>('')
-    const [ password, setPassword ] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
     // email and password validity
-    const [ validEmail, setValidEmail ] = useState<boolean>(false)
-    const [ validPassword, setValidPassword ] = useState<boolean>(false)
+    const [validEmail, setValidEmail] = useState<boolean>(false)
+    const [validPassword, setValidPassword] = useState<boolean>(false)
 
-    useEffect( () => {
-        if( email.indexOf('@') > 0 ) {
+    useEffect(() => {
+        if (email.indexOf('@') > 0) {
             // console.log('valid email')
-            setValidEmail( true )
+            setValidEmail(true)
         }
         else {
             // console.log('invalid email')
-            setValidEmail( false )
+            setValidEmail(false)
         }
-    }, [email] )
+    }, [email])
 
-    useEffect( () => {
-        if( password.length >= 8 ) {
+    useEffect(() => {
+        if (password.length >= 8) {
             // valid password
-            setValidPassword( true )
+            setValidPassword(true)
         }
         else {
             // invalid password
-            setValidPassword( false )
+            setValidPassword(false)
         }
     }, [password])
 
     return (
         <ThemedView style={styles.container}>
-            <View style={ styles.form }>
+            <View style={styles.form}>
                 <Text style={styles.title}>Sign up</Text>
-                <ThemedText>Email</ThemedText>
-                <TextInput 
-                    style={styles.input} 
-                    placeholder='you@example.com' 
-                    onChangeText={ (val) => setEmail(val) }
+                <View style={ styles.label }>
+                    <ThemedText>Email</ThemedText>
+                    <ValidIndicator valid={validEmail} />
+                </View>
+                
+                <TextInput
+                    style={styles.input}
+                    placeholder='you@example.com'
+                    onChangeText={(val) => setEmail(val)}
                     value={email}
                 />
-                <ThemedText>Password</ThemedText>
-                <TextInput 
-                    style={styles.input} 
+
+                <View style={ styles.label }>
+                    <ThemedText>Password</ThemedText>
+                    <ValidIndicator valid={validPassword} />
+                </View>
+                <TextInput
+                    style={styles.input}
                     placeholder='minimum 8 characters'
-                    secureTextEntry={ true }  
+                    secureTextEntry={true}
                     value={password}
-                    onChangeText={ (val) => setPassword(val) }
+                    onChangeText={(val) => setPassword(val)}
                 />
-                <Pressable 
-                    style={ (validEmail && validPassword ) ? styles.button : styles.buttondisabled } 
-                    disabled={ ( validEmail && validPassword ) ? false : true }
+
+                <Pressable
+                    style={(validEmail && validPassword) ? styles.button : styles.buttondisabled}
+                    disabled={(validEmail && validPassword) ? false : true}
                 >
-                     <ThemedText style={ styles.buttonText }>Sign up</ThemedText>
+                    <ThemedText
+                        style={(validEmail && validPassword) ? styles.buttonText : styles.buttonTextDisabled}
+                    >
+                        Sign up
+                    </ThemedText>
                 </Pressable>
             </View>
         </ThemedView>
@@ -81,7 +95,7 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
-        
+
     },
     button: {
         borderStyle: "solid",
@@ -93,11 +107,19 @@ const styles = StyleSheet.create({
     buttondisabled: {
         borderStyle: "solid",
         borderWidth: 2,
-        borderColor: "#7e848c",
+        borderColor: "#45474a",
         padding: 5,
         borderRadius: 5,
     },
     buttonText: {
         textAlign: "center",
+    },
+    buttonTextDisabled: {
+        textAlign: "center",
+        color: "#45474a"
+    },
+    label: {
+        flexDirection: "row",
+        justifyContent: "space-between"
     },
 })
