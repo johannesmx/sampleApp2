a4a779c5a1036220ea75740c4e242c99c252f0cf
johannes <jmuljana@gmail.com>
2025-07-25 00:06:36 +1000

    added dist file for config

---
+++ config/Config.dist.ts
@@ -0,0 +1,4 @@
+export const EXPO_PUBLIC_APPWRITE_ENDPOINT = "https://fra.cloud.appwrite.io/v1"
+export const EXPO_PUBLIC_APPWRITE_PROJECT_ID = ""
+export const DATABASE_ID = ""
+export const COLLECTION_ID = ""
